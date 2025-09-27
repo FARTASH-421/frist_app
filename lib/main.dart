3ad43277ffc2105b09039b1328407e5c1d63abc0
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-09-28 00:59:25 +0330

    in Widget change to Extract Widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,55 +21,66 @@ class MyApp extends StatelessWidget {
       supportedLocales: [
         Locale('fa'), // Persion
       ],
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          actions: [
-            SizedBox(width: 16.0),
-            Image.asset('assets/images/icon.png'),
-            SizedBox(width: 12),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'قیمت به‌روز ارز',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          SizedBox(width: 16.0),
+          Image.asset('assets/images/icon.png'),
+          SizedBox(width: 12),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'قیمت به‌روز ارز',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Image.asset('assets/images/menu.png'),
-              ),
-            ),
-            SizedBox(width: 16.0),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset('assets/images/q.png'),
-                  SizedBox(width: 12.0),
-                  Text(
-                    'نرخ ارز آزاد چیست؟ ',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text(
-                ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
-                style: TextStyle(fontSize: 14, color: Colors.black),
-                textDirection: TextDirection.rtl,
-              ),
-            ],
           ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset('assets/images/menu.png'),
+            ),
+          ),
+          SizedBox(width: 16.0),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/images/q.png'),
+                SizedBox(width: 12.0),
+                Text(
+                  'نرخ ارز آزاد چیست؟ ',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
+              style: TextStyle(fontSize: 14, color: Colors.black),
+              textDirection: TextDirection.rtl,
+            ),
+          ],
         ),
       ),
     );

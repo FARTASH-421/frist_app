30be86b21c2d1b6cfe815deb701386f8302efd1c
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-09-28 14:25:46 +0330

    creact ListView biluder for show data

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Vazirmatn'),
+      theme: ThemeData(
+        fontFamily: 'Vazirmatn',
+        textTheme: TextTheme(
+          headlineMedium: TextStyle(
+            fontFamily: 'Vazirmatn',
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: 'Vazirmatn',
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
@@ -27,15 +41,16 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({
-    super.key,
-  });
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 243, 243, 243),
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
+
         actions: [
           SizedBox(width: 16.0),
           Image.asset('assets/images/icon.png'),
@@ -44,7 +59,7 @@ class Home extends StatelessWidget {
             alignment: Alignment.centerRight,
             child: Text(
               'قیمت به‌روز ارز',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ),
           Expanded(
@@ -67,18 +82,98 @@ class Home extends StatelessWidget {
                 SizedBox(width: 12.0),
                 Text(
                   'نرخ ارز آزاد چیست؟ ',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 18),
             Text(
               ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
-              style: TextStyle(fontSize: 14, color: Colors.black),
+              style: Theme.of(context).textTheme.bodyMedium,
               textDirection: TextDirection.rtl,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: Container(
+                height: 34,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 133, 133, 133),
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'نام آزاد ارز',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'قیمت',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'تغییر',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Container(
+              height: 400,
+              width: double.infinity,
+              // color: Colors.lightBlueAccent,
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int postion) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 8.0, right: 2, left: 2),
+                    child: Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(color: Colors.grey, blurRadius: 4),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "دلار",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            "25000",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            "+8",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
